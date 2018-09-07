fc7dbdac70c90ce63cc8897a737b7004887edc17
vanchopi <ivan.ugnenko@webregul.ru>
2018-09-07 19:51:53 +0300

    seems done

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 	  slidesToScroll: 4,
 	  responsive: [
 	    {
-	      breakpoint: 1024,
+	      breakpoint: 1715,
 	      settings: {
 	        slidesToShow: 3,
 	        slidesToScroll: 3,
@@ -17,14 +17,14 @@ $(document).ready(function(){
 	      }
 	    },
 	    {
-	      breakpoint: 600,
+	      breakpoint: 1340,
 	      settings: {
 	        slidesToShow: 2,
 	        slidesToScroll: 2
 	      }
 	    },
 	    {
-	      breakpoint: 480,
+	      breakpoint: 992,
 	      settings: {
 	        slidesToShow: 1,
 	        slidesToScroll: 1
